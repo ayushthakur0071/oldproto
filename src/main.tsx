88b9c0c2380899809b6f70f40ed8b5ce6fa9e192
Ayush Thakur <ayushthakur@Ayushs-MacBook-Air.local>
2026-04-18 18:55:57 +0100

    fix router basename

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
+<BrowserRouter basename="/oldproto">
+  <App />
+</BrowserRouter>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
